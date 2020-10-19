743342751b9edeff67657ffcdc61fd7bae15e429
mplowey28 <matthew.plowey@gmail.com>
2020-10-19 12:01:01 +0200

    added nav component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Nav from './Nav';
 
 library.add(fas);
 
 
 const Header = () => {
     return (
-        <div className="container">
-            <a href="#" className="logo">
-            <FontAwesomeIcon icon="utensils" />
-                Restaurant
-            </a>
-            <a href="#">Call us on +00 00 000 0000</a>
-        </div>
+        <header>
+            <div className="container">
+                <a href="#" className="logo">
+                <FontAwesomeIcon icon="utensils" />
+                    Restaurant
+                </a>
+                <a href="#">Call us on +00 00 000 0000</a>
+            </div>
+            <Nav />
+        </header>
     )
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <ul>
+            <li><a href="#" className="active">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#" className="highlight">Book a Table</a></li>
+        </ul>
+    )
+}
+
+export default Nav
